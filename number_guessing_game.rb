1a6b6c7d01df22a34bc55738356e981251b2256a
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-09-29 15:25:06 -0500

    Finish number guessing game

--- number_guessing_game.rb
+++ number_guessing_game.rb
@@ -12,21 +12,50 @@
 # The computer gets five guesses.
 
 def number_guess
-  a = rand(1..100)
+  a = rand(1..100).to_i
+  p a
 
   print "Take a guess my friend: "
   response = gets.chomp
 
-  until a == response
-
+  until response.to_i == a
       if response.to_i > a
         puts "Your number is greater than number. Guess again"
         response = gets.chomp
-      else
+
+      elsif response.to_i < a
         puts "Your number is smaller than number. Guess again"
         response = gets.chomp
       end
-    end
+  end
+
+  p "You are a great Gusser!"
+
 end
 
-number_guess()
+
+
+def i_pick
+
+  p "Let me pick a number from 1 to 100:"
+  user = gets.chomp
+
+  a = rand(1..100)
+  count = 0
+
+  until a == user.to_i || count > 5
+    if a > user.to_i
+      p "#{a} is too high"
+      a = rand(1..100)
+    elsif a < user.to_i
+      p "#{a} is too low"
+      a = rand(1..100)
+    elsif a == user.to_i
+      p "I got u right"
+    end
+    count += 1
+  end
+
+end
+
+i_pick()
