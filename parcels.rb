bee793efeb9f289b63ad3ed023f522f643a8aab5
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-10-05 23:07:32 -0500

    Move volume method, cost_to_ship method, ask method and calculate method in User class

--- parcels.rb
+++ parcels.rb
@@ -5,77 +5,81 @@
 # Build a program that lets users calculate shipping costs.
 
 class Parcel
-	attr_accessor :dimensions, :weight, :cost, :he, :we
+	attr_accessor :height, :weight, :he, :we
 
 	def initialize(height,weight)
 		@height = height
 		@weight = weight
-		@cost = 0
-	end
-
-	def volume
-		p "You want to ship a parcel #{@height} ft and #{@weight} pounds"
-	end
-
-	def cost_to_ship
-		@cost = @height.to_i * 5 + @weight.to_i * 10
-		p "The fee of shipping is $5 for 1 feet and $10 for 1 pounds"
-		p "Your total cost would be $#{@cost}"
-	end
-
-	def ask
-		p "Please enter height"
-		he = gets.chomp
-		p "Please enter weight"
-		we = gets.chomp
-		total = he.to_i * 5 + we.to_i * 10
-		p "Your total will be #{total}"
-	end
-
-	def calculate
-		puts "\r"
-		p "Do you want to count shipping cost:? y for yes e for exit"
-		answer = gets.chomp
-		if answer == "y"
-			ask()
-		else
-			return
-		end
-
-		puts "\r"
-		p "Do you want to keep calculating: y for yes, e for exit"
-		answer = gets.chomp
-		if answer == "y"
-			ask()
-		else
-			return
-		end
 	end
 end
 
 class User
-	attr_accessor :name, :he, :we
-	@@products = []
+    attr_accessor :name, :he, :we, :cost, :parcel
+	
 
 	def initialize(name)
 		@name = name
 		@he = he
 		@we = we
-		User.question
+        @cost = 0
+        @parcel = []
+		
 	end
 
-	def self.question
+	def volume
+		p "Welcome to our shop"
 		p "Please enter height of your parcel"
 		he = gets.chomp
 		p "Please enter weight of your parcel"
 		we = gets.chomp
-		p "You want to ship a parcel #{he} ft and #{we} pounds"
-		new = Parcel.new(he,we)
-		new.volume
-		new.cost_to_ship
-		new.calculate
-	end
-end
+        
+        @parcel  =  Parcel.new(he,we)
 
+        p "You want to ship a parcel #{@parcel.height} ft and #{@parcel.weight} pounds"
+
+    end
+        
+   
+    def cost_to_ship
+        @cost = @parcel.height.to_i * 5 + @parcel.weight.to_i * 10
+        p "The fee of shipping is $5 for 1 feet and $10 for 1 pounds"
+        p "Your total cost would be $#{@cost}"
+    end
+    
+    def ask
+        p "Please enter height"
+        he = gets.chomp
+        p "Please enter weight"
+        we = gets.chomp
+        total = he.to_i * 5 + we.to_i * 10
+        p "Your total will be $#{total}"
+    end
+        
+    def calculate
+        puts "\r"
+        p "Do you want to count shipping cost:? y for yes e for exit"
+        answer = gets.chomp
+
+        if answer == "y"
+            ask()
+        else
+            return
+        end
+            
+        puts "\r"
+        p "Do you want to keep calculating: y for yes, e for exit"
+        answer = gets.chomp
+
+        if answer == "y"
+            ask()
+        else
+            return
+        end
+    
+end
+end
 thanh = User.new :Thanh
+thanh.volume
+thanh.cost_to_ship
+thanh.calculate
 
